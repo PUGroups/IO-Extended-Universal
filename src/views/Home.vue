4dadb5b51eab6bb3857dd354bd15c7c46be30e3d
Pulkit Singh <singh.pulkit.0076@gmail.com>
2021-07-20 21:46:07 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,7 @@
     <div class="container">
       <div class="row my-1 m-auto">
         <div class="col my-3 box">
-          <h1>Why <span>IO Extended ?</span></h1>
+          <h1 id="about" >Why <span>IO Extended ?</span></h1>
         </div>
       </div>
       <div class="row my-1 d-flex justify-content-around">
@@ -136,7 +136,7 @@
 
     <!-- ______________SPEAKERS LINEUP___________START -->
 
-    <div class="container mt-5">
+    <div class="container mt-5" id="speaker" >
       <div class="row mt-5">
         <div class="col m-auto box d-flex justify-content-center">
           <h1 class="mb-5">Speakers <span id="IO">Lineup </span></h1>
@@ -246,7 +246,7 @@
 
     <!-- ______________PARTNERS COMMUNITIES__________START -->
 
-    <div class="container mt-5">
+    <div class="container mt-5" id="community" >
       <div class="row mt-5">
         <div class="col box m-auto d-flex justify-content-center">
           <h1 class="heading mb-4">
