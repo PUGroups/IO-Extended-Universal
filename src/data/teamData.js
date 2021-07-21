adc260d34c3c23997cab76c9a262e622f22fa80c
Pulkit Singh <singh.pulkit.0076@gmail.com>
2021-07-21 21:26:42 +0530

    Update teamData.js

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -2,12 +2,12 @@ const teamData = [
   {
     id: 1,
     img:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThIE_WG-ywUwymdSlOZfw4q0mgj5EjaIwBqQ&usqp=CAU",
-    name: "Menu Benu",
-    position: "Founder",
-    twitter: "https://www.google.com/",
-    linkdin: "https://www.google.com/",
-    github: "https://www.google.com/",
+      "https://pbs.twimg.com/profile_images/1402270441922871297/hGRphzr5_400x400.jpg",
+    name: "Pulkit Singh",
+    position: "Lead Organizer",
+    twitter: "https://twitter.com/PulkitSinghDev",
+    linkdin: "https://www.linkedin.com/in/PulkitSinghDev/",
+    github: "https://github.com/PulkitSinghDev",
   },
 
   {
