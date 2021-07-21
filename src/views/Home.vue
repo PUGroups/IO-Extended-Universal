78ecb47760e3da75d3338960653f6e865026a076
Pulkit Singh <singh.pulkit.0076@gmail.com>
2021-07-21 15:37:49 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -237,7 +237,7 @@
         <div class="col-4 d-flex justify-content-center align-items-center">
           <a href="http://discord.pugroups.in">
             <img
-              src="../assets/community/pug.png"
+              src="../assets/community/PUGroups_Logo.png"
               width="150"
               class="img-fluid"
               alt=""
