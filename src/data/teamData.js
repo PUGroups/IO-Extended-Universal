dc99b8e82216cd04ee73614723e51ddf86e09e61
Pulkit Singh <singh.pulkit.0076@gmail.com>
2021-07-22 08:27:32 +0530

    Update teamData.js

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -13,6 +13,28 @@ const teamData = [
   {
     id: 2,
     img:
+      "https://pbs.twimg.com/profile_images/1353746199824773122/9ZLmxxxL_400x400.jpg",
+    name: "Kartikey Rawat",
+    position: "Organizer",
+    twitter: "https://twitter.com/carrycooldude",
+    linkdin: "https://www.linkedin.com/in/carrycooldude/",
+    github: "https://github.com/carrycooldude",
+  },
+  
+  {
+    id: 3,
+    img:
+      "https://pbs.twimg.com/profile_images/1367940033936728065/bOEHv1CD_400x400.jpg",
+    name: "Khushi Sharma",
+    position: "Organizer",
+    twitter: "https://twitter.com/khushiSharma_22",
+    linkdin: "https://www.linkedin.com/in/khushi-sharma-2201/",
+    github: "",
+  },
+  
+  {
+    id: 4,
+    img:
       "https://i.ibb.co/8sFMnwY/2ab258c3-d22c-4b73-929b-36039ce09c14.jpg",
     name: "Aashu",
     position: "",
