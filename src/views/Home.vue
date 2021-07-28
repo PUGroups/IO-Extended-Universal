6a086c5d4ce4bf7d1e2101bce2db08ca3d1cd8a8
Aashutosh Anand Tiwari <40313523+ashumsd7@users.noreply.github.com>
2021-07-28 14:38:25 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,10 +24,12 @@
           </div>
           <h2 id="tag2">Experience IO</h2>
           <h2 id="tag3">With your local communties</h2>
-          <a href="https://forms.gle/PCJhTisg1p3S2Mdb6" target="_blank" >
-          <button class="bg-light register-btn p-2 mt-3 outline-none border-0">
-            Register Now
-          </button>
+          <a href="https://forms.gle/PCJhTisg1p3S2Mdb6" target="_blank">
+            <button
+              class="bg-light register-btn p-2 mt-3 outline-none border-0"
+            >
+              Register Now
+            </button>
           </a>
         </div>
       </div>
@@ -35,324 +37,53 @@
       <div class="row d-lg-block design-banner-circle"></div>
     </div>
 
-    <!-- ______________WHY IO EXTENDED___________START -->
+    <AboutIO />
 
-    <div class="container" id="about" >
-      <div class="row my-1 m-auto">
-        <div class="col my-3 box">
-          <h1>Why <span>IO Extended ?</span></h1>
-        </div>
-      </div>
-      <div class="row my-1 d-flex justify-content-around">
-        <div class="col-md-5 m-2 p-3 community-div">
-          <h2 class="mb-3 mt-2">Because Community matters</h2>
-          <div class="row">
-            <div class="col-lg-10">
-              <p>
-                Come together with community because alone we can do so little but together with community we can grow and reach to great heights.United with Community one can make a remarkable changes in their personal growth and also towards society!
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-5 m-2 p-2 growth-div">
-          <h2 class="mb-3 mt-2">Because Growth matters</h2>
-          <div class="row">
-            <div class="col-lg-10">
-              <p>
-                Growth is a process not an event. Being more and more productive each day is growth.Grow remarkably and upscale your level of potential output with experience holders as every experience makes you grow.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+    <CelebrationOfTech />
 
-      <div class="row my-2 d-flex justify-content-around">
-        <div class="col-md-8 m-2 p-3 you-matter-div">
-          <h2 class="mt-2">Because YOU matter</h2>
-          <p>
-            You matter, You are important, You can evolve, You contribute in development. Get inspired by the speakers at I/O extended and start working on yourselves. You can affect the world fundamentally, technically and revolutionarily.
-          </p>
-        </div>
-        <div class="col-md-2 m-1 p-3 calender-div text-center">
-          <img src="../assets/CalendarIcon.png" alt="" />
-          <h2 class="mt-3">31st July 2021</h2>
-          <h3>Saturday</h3>
-        </div>
-      </div>
-    </div>
-
-    <!-- ______________WHY IO EXTENDED___________END -->
-
-    <!-- ______________CELEBRATION OF TECH___________START -->
-
-    <div class="container mt-5">
-      <div class="row mt-5">
-        <div class="col m-auto mb-4 box d-flex justify-content-center">
-          <h1 class="heading">
-            A Local <span id="IO">Celebration of Tech </span>
-          </h1>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col m-auto partners-img d-flex justify-content-center">
-          <img src="../assets/partners.png" alt="" class="img-fluid" />
-        </div>
-      </div>
-    </div>
-    <!-- ______________CELEBRATION OF _________END -->
-    <!-- ______________EVENT AGENDA_______start -->
-    <!-- <div class="container mt-5">
-      <div class="row mt-5">
-        <div class="col box m-auto d-flex justify-content-center">
-          <h1 class="heading mb-4">
-            Event <span id="IO">Agenda </span>
-          </h1>
-        </div>
-      </div>
-      <div class="row ">
-       <events></events>
-      </div>
-    </div> -->
-
-    <!-- ________EVENT AGENDA END________ -->
-
-    <!-- ______________SPEAKERS LINEUP___________START -->
-
-    <div class="container mt-5" id="speaker" >
+    <div class="container mt-5" id="speaker">
       <div class="row mt-5">
         <div class="col m-auto box d-flex justify-content-center">
-          <h1 class="mb-5">Speakers <span id="IO">Lineup </span></h1><br>
-        </div>
-      </div>
-    
-    </div>
-    
-    <!-- ______________SPEAKERS LINEUP___________END -->
-
-    <!-- ______________community collaboration__________START -->
-    <div class="container mt-5" id="community" >
-      <div class="row mt-5">
-        <div class="col box m-auto d-flex justify-content-center">
-          <h1 class="heading mb-4">
-            Community <span id="IO"> Collaboration </span>
-          </h1>
-        </div>
-      </div>
-      <div class="row mx-3 d-flex justify-content-around align-items-center">
-        <div class="col-4 d-flex justify-content-center align-items-center">
-          <a href="http://discord.pugroups.in">
-            <img
-              src="../assets/community/pug.png"
-              width="150"
-              class="img-fluid"
-              alt=""/>
-          </a>
-        </div>
-        <div class="col-4 d-flex justify-content-center align-items-center">
-          <a href="https://developerstudentcommunity.co">
-            <img
-              src="../assets/community/dsc.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
+          <h1 class="mb-5">Speakers <span id="IO">Lineup </span></h1>
+          <br />
         </div>
       </div>
     </div>
-
-    <!-- ______________community collaboration__________END -->
-
-    <!-- ______________PARTNERS COMMUNITIES__________START -->
-
-    <div class="container  mt-5">
-      <div class="row mt-5">
-        <div class="col box m-auto d-flex justify-content-center">
-          <h1 class="heading mb-4">
-            Partner <span id="IO">Communities </span>
-          </h1>
-        </div>
-      </div>
-      <div class="row mb-5  d-flex justify-content-center align-items-center">
-        <div class="col-3 d-flex justify-content-center align-items-center ">
-          <a href="https://gdg.community.dev/gdg-chandigarh">
-            <img
-              src="https://i.ibb.co/52tq2BC/gdgchan.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-      <div class="col-3 d-flex justify-content-center align-items-center">
-          <a href="https://gdg.community.dev/gdg-san-francisco/">
-            <img
-              src="../assets/community/cloud2.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-
-        <div class="col-3 d-flex justify-content-center align-items-center">
-          <a href="http://">
-            <img
-              src="../assets/community/cloud3.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-
-        <div class="col-3 d-flex justify-content-center align-items-center">
-          <a href="http://">
-            <img
-              src="../assets/community/cloud4.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-        
-        <div class="col-3 mt-2 d-flex justify-content-center align-items-center">
-          <a href="https://dscnitrourkela.org/">
-            <img
-              src="../assets/community/NITRkela.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-
-        <div class="col-3 mt-2 d-flex justify-content-center align-items-center">
-          <a href="https://dsc.community.dev/amity-university-noida/">
-            <img
-              src="../assets/community/DSC_amity.jpg"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-        
-        <div class="col-3 mt-2 d-flex justify-content-center align-items-center">
-          <a href="https://flutterchd.web.app/">
-            <img
-              src="https://pbs.twimg.com/profile_images/1322962893420130305/lAwGa4IY_400x400.jpg"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-        <div class="col-3 d-flex justify-content-center align-items-center">
-          <a href="https://twitter.com/commclassroom">
-            <img
-              src="../assets/community/c.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-        <div class="col-3 d-flex justify-content-center align-items-center">
-          <a href="https://www.meetup.com/tensorflow-user-group-chandigarh/">
-            <img
-              src="../assets/community/tensorflow.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-        
-        
-        <div class="col-4 d-flex justify-content-center align-items-center">
-          <a href="https://linktr.ee/Equicode">
-            <img
-              src="../assets/community/code.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-   
-
-     
-        <div class="col-3 mt-2 d-flex justify-content-center align-items-center">
-          <a href="https://community.cncf.io/navi-mumbai/">
-            <img
-              src="../assets/community/navimumbai.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-        <div class="col-3 mt-2 d-flex justify-content-center align-items-center">
-          <a href="https://community.codecademy.com/navi-mumbai/">
-            <img
-              src="../assets/community/c_.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-       <div class="col-3 mt-2 d-flex justify-content-center align-items-center">
-          <a href="https://linktr.ee/codefloworg">
-            <img
-              src="../assets/community/codeflow.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-
-        <div class="col-3 mt-2 d-flex justify-content-center align-items-center">
-          <a href="https://twitter.com/basecampXD">
-            <img
-              src="../assets/community/flag_logo.png"
-              width="150"
-              class="img-fluid"
-              alt=""
-          /></a>
-        </div>
-      </div>
+    <div v-if="!XX" style="justify-content: center">
+      <h3 class="text-center mark">Anouncing soon....</h3>
     </div>
 
-    <!-- ______________PARTNERS COMMUNITIES__________END -->
+    <Speakers v-else />
+
+    <CommunityCollab />
+
+    <PartnerCommunity />
   </div>
 </template>
 
 <script>
 import Events from "./Events.vue";
+import CommunityCollab from "../components/HomeViews/CommunityCollab.vue";
+import PartnerCommunity from "../components/HomeViews/PartnerCommunity.vue";
+import Speakers from "../components/HomeViews/Speakers.vue";
+import CelebrationOfTech from "../components/HomeViews/CelebrationOfTech.vue";
+import AboutIO from "../components/HomeViews/AboutIO.vue";
 export default {
   components: {
     Events,
+    CommunityCollab,
+    PartnerCommunity,
+    Speakers,
+    CelebrationOfTech,
+    AboutIO,
   },
 };
 </script>
 <style  scoped>
 @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
-/* @import url('https://fonts.googleapis.com/css2?family=Coda+Caption:wght@800&display=swap'); */
-@import url("https://fonts.googleapis.com/css2?family=Poppins&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap");
 
-/* _________MAIN_HEADER_START___________ */
-/* .main-header h1 { */
-/* font-weight: bolder; */
-/* font-family: "Bebas Neue", cursive;
-  color: rgb(0, 0, 0);
-  font-weight: bolder;
-  font-size: 5rem; */
-/* font-family: 'Ultra', serif; */
-/* font-family: 'Coda Caption', sans-serif; */
-/* font-family: "Poppins", sans-serif;
-} */
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap");
 
 #logo {
   width: 40px;
@@ -405,9 +136,6 @@ export default {
   .design-banner-circle {
     display: none;
   }
-  .calender-div {
-    max-width: 300px;
-  }
 }
 
 .main-header #tag1 {
@@ -429,78 +157,4 @@ export default {
   font-family: "Poppins", sans-serif;
 }
 /* _________MAIN_HEADER_END___________ */
-
-/* __________MATTERS DIV START_________ */
-.col {
-  font-family: "Open Sans", sans-serif;
-}
-h1 span {
-  font-weight: bolder;
-}
-
-.box h1 {
-  font-family: "Poppins", sans-serif;
-}
-.community-div {
-  background: #f88c84;
-}
-.community-div,
-.growth-div,
-.you-matter-div,
-.calender-div {
-  border-radius: 30px;
-  /* box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
--webkit-box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66);
--moz-box-shadow: 10px 14px 17px -8px rgba(0,0,0,0.66); */
-  font-family: "Poppins", sans-serif;
-}
-
-.community-div p,
-.growth-div p {
-  margin: 10px;
-  font-weight: 600;
-  font-family: "Roboto", sans-serif;
-}
-.growth-div {
-  background: #88cc94;
-}
-.you-matter-div {
-  background: #ffbc04;
-}
-.calender-div {
-  background: #3b8fff;
-  color: wheat;
-}
-
-/* ___CEO */
-.ceo img {
-  border-radius: 50%;
-}
-
-/* __________MATTERS DIV END */
-
-/* ______CELEBRATION OF TECH START________ */
-
-#IO {
-  font-weight: 900;
-}
-/* .heading {
-  font-weight: bolder;
-  font-size: 2rem;
-} */
-/* ______CELEBRATION OF TECH END */
-
-/* _____EVENT START____ */
-.event_icons {
-  margin-left: -80px;
-}
-/* _____EVENT END______ */
-
-/* _____mix */
-
-@media screen and (max-width: 750px) {
-  .calender-div {
-    max-width: 160px;
-  }
-}
 </style>
