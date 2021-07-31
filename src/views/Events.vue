45f19c49588db94abb18fde90737a338cb627d35
Aashutosh Anand Tiwari <40313523+ashumsd7@users.noreply.github.com>
2021-07-31 14:07:14 +0530

    Update Events.vue

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -22,8 +22,8 @@
     <div class="card col-lg-4 col-md-6 px-2   pb-4 mx-1 mt-5" v-for="data in getEventTImeLine" :key="data.eventId">
         <div class="row"> <img class="img-fluid speaker-img mt-2 m-auto"  :src="data.profilePicture">
             <h2 class=" ml-2 mt-1 speaker-name text-center">{{data.speakersName}}</h2>
-            <!-- <div  class="text-center timings"><span class="text-success text-center">Starts at {{data.slotStart}}</span></div> -->
-            <div  class="text-center timings"><span class="text-success text-center">Event is Live</span></div>
+            <div  class="text-center timings"><span class="text-success text-center">Watch between {{data.slotStart}}</span></div>
+            <!-- <div  class="text-center timings"><span class="text-success text-center">Event is Live</span></div> -->
         </div>
         <p class="mt-3 mb-2"> Today </p>
         <h5 class="title">{{data.title}}</h5>
@@ -37,7 +37,7 @@
              <img src="https://imgur.com/P5kPDPG.png">
               </div> -->
                <button class="btn btn-success mb-2 mt-3 reminder">
-                <span class=""><a :href="url">Click to Watch</a></span>
+                <span class=""><a :href="url">Watch Event Live</a></span>
                 
                 </button>
                  <!-- <button class="btn info"><span>Watch </span></button> -->
